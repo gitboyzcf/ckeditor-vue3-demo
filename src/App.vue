9bc8d7ce721c0ea7499e23610cc4712211e9bbc5
zhangchenfei <boyzcf@qq.com>
2023-07-28 15:42:55 +0800

    🎉 init: 添加富文本demo

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { reactive } from "vue";
+import CKEditor from "@ckeditor/ckeditor5-vue";
+import ClassicEditor from "ckeditor5-build-classic";
+
+const state = reactive({
+  editor: ClassicEditor,
+  editorData: "<p>Content of the editor.</p>",
+  editorConfig: {
+    // The configuration of the editor.
+  },
+});
+</script>
+
+<template>
+  <main>
+    <CKEditor.component
+      :editor="state.editor"
+      v-model="state.editorData"
+      :config="state.editorConfig"
+    ></CKEditor.component>
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  width: 800px;
+  height: 600px;
+  margin: 50px auto;
+}
+</style>
+<style lang="scss">
+.ck.ck-content{
+  height: 400px;
+}
+
+</style>
